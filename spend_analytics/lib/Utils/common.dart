82e21b106d13a8fd192abe04a024ba79b044cda6
Sumit Pal <36818574+Sam21-39@users.noreply.github.com>
2020-09-05 15:01:43 +0530

    Update common.dart

--- spend_analytics/lib/Utils/common.dart
+++ spend_analytics/lib/Utils/common.dart
@@ -4,6 +4,10 @@ num setFontSize(size) {
   return ScreenUtil().setSp(size, allowFontScalingSelf: true);
 }
 
+num setSize(size) {
+  return ScreenUtil().setSp(size);
+}
+
 num setWidth(size) {
   return ScreenUtil().setWidth(size);
 }
