02f964b3042d4637cdfde318d0cd76918914983d
Sumit Pal <36818574+Sam21-39@users.noreply.github.com>
2020-09-09 16:30:39 +0530

    Update ui_text.dart

--- spend_analytics/lib/Views/UI/ui_text.dart
+++ spend_analytics/lib/Views/UI/ui_text.dart
@@ -4,6 +4,7 @@ import 'package:spend_analytics/Utils/common.dart';
 class UiText {
   static final TextStyle headerText = new TextStyle(
     fontFamily: 'Roboto',
+    fontWeight: FontWeight.w900,
     fontSize: setFontSize(25.0),
     height: setHeight(1.5),
   );
@@ -11,6 +12,13 @@ class UiText {
   static final TextStyle normalText = new TextStyle(
     fontFamily: 'Roboto',
     fontSize: setFontSize(20.0),
+    height: setHeight(1.5),
+  );
+
+  static final TextStyle buttonText = new TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: setFontSize(20.0),
+    fontWeight: FontWeight.w600,
     height: setHeight(1.5),
   );
 }
