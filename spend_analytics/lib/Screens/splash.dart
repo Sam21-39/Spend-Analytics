ba4f64747a8fd84ac6b30d7be4fc4d2f246b5c15
Sumit Pal <36818574+Sam21-39@users.noreply.github.com>
2020-09-12 23:32:29 +0530

    new updates

--- spend_analytics/lib/Screens/splash.dart
+++ spend_analytics/lib/Screens/splash.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
 class Splash extends StatefulWidget {
   @override
   _SplashState createState() => _SplashState();
@@ -8,7 +10,10 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      color: Color(0xfff2f2f2),
+      child: SvgPicture.asset('assets/images/food.svg'),
     );
   }
 }
