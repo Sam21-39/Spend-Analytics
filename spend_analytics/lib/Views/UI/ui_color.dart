202d33b3c33c772ce03bd80c581e29af026b2b14
Sumit Pal <36818574+Sam21-39@users.noreply.github.com>
2020-09-12 23:00:41 +0530

    updated Ui and app

--- spend_analytics/lib/Views/UI/ui_color.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class UiColor {
-  static final Color primary = Colors.teal.shade400;
-  static final Color dark = Color(0xff484848);
-  static final Color light = Colors.white;
-}
--- spend_analytics/lib/main.dart
+++ spend_analytics/lib/main.dart
@@ -1,34 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:spend_analytics/Views/Screens/splash.dart';
-import 'package:spend_analytics/Views/UI/ui_color.dart';
+import 'package:spend_analytics/Screens/splash.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Spend Analytics',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        accentColor: UiColor.light,
-        appBarTheme: AppBarTheme(
-          elevation: 0.0,
-          color: UiColor.light,
-          iconTheme: IconThemeData(color: UiColor.dark),
-        ),
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: UiColor.light,
-          foregroundColor: UiColor.light,
-          elevation: 12.0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(100.0),
-            side: BorderSide(color: Colors.white30),
-          ),
-        ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Splash(),
